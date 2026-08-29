56fafef1194363fd94a5b7f6906527853650d7c5
Ray <chan_ray@ymail.com>
2026-08-30 01:47:15 +0800

    Added todo, deadline and events

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,14 @@
+public class Deadline extends Task{
+
+    private String due;
+
+    public Deadline(String name, String endTime) {
+        super(name);
+        this.due = endTime;
+    }
+
+    @Override
+    public String toString() {
+        return "[D] " + super.toString() + " (due: " + due + ")";
+    }
+}
--- src/main/java/Kevie.java
+++ src/main/java/Kevie.java
@@ -18,8 +18,13 @@ public class Kevie {
     public static final String PREFIX_INDENT = "  ";
     public static final int LIST_MAX_LEN = 100;
 
+    public static void botSay(String msg, boolean identOnly) {
+        if (!identOnly) System.out.println(PREFIX_BOT + " " + msg);
+        else System.out.println(PREFIX_INDENT + " " + msg);
+    }
+
     public static void botSay(String msg) {
-        System.out.println(PREFIX_BOT + " " + msg);
+        botSay(msg, false);
     }
 
     public static void main(String[] args) {
@@ -44,62 +49,120 @@ public class Kevie {
                 case "list":
                     botSay("Ok my guy, here is your list:");
                     taskList.printAll(PREFIX_INDENT);
-                    continue;
+                    break;
                 case "mark":
-                    try {
-                        int markNo = Integer.parseInt(inputArgs[1]);
-                        if (markNo > taskList.getLength())
-                        {
-                            botSay("There are only " + taskList.getLength() + " tasks, yet you want to mark task " + markNo
-                                    + " as done? Try again bro.");
-                            continue;
-                        }
-                        else if (markNo < 1)
-                        {
-                            botSay("Your task number to be marked done must be positive ahh!!!");
-                            continue;
-                        }
+                    int markNo = -1;
 
-                        Task markedTask = taskList.getTask(markNo - 1);
-                        markedTask.setDone(true);
-                        botSay("Ok! I have marked a task as done: ");
-                        System.out.println(PREFIX_INDENT + markedTask.toString());
-                        continue;
+                    try {
+                        markNo = Integer.parseInt(inputArgs[1]);
 
                     } catch (Exception e) {
                         botSay("I cannot mark anything if you dont give me a number lol.");
-                        continue;
+                        break;
                     }
-                case "unmark":
-                    try {
-                        int markNo = Integer.parseInt(inputArgs[1]);
-                        if (markNo > taskList.getLength())
-                        {
-                            botSay("There are only " + taskList.getLength() + " tasks, yet you want to unmark task " + markNo
-                                    + "? Try again bro.");
-                            continue;
-                        }
-                        else if (markNo < 1)
-                        {
-                            botSay("Your task number to be undone must be positive ahh!!!");
-                            continue;
-                        }
 
-                        Task markedTask = taskList.getTask(markNo - 1);
-                        markedTask.setDone(false);
-                        botSay("Ok! I have marked a task as undone:");
-                        System.out.println(PREFIX_INDENT + markedTask.toString());
-                        continue;
+                    if (markNo > taskList.getLength())
+                    {
+                        botSay("There are only " + taskList.getLength() + " tasks, yet you want to mark task "
+                                + markNo + " as done? Try again bro.");
+                        break;
+                    }
+                    else if (markNo < 1)
+                    {
+                        botSay("Your task number to be marked done must be positive ahh!!!");
+                        break;
+                    }
+
+                    Task markedTask = taskList.getTask(markNo - 1);
+                    markedTask.setDone(true);
+                    botSay("Ok! I have marked a task as done: ");
+                    botSay(markedTask.toString(), true);
+                    break;
+                case "unmark":
+                    int unmarkno = -1;
+
+                    try {
+                        unmarkno = Integer.parseInt(inputArgs[1]);
                     }
                     catch(NumberFormatException e) {
                         botSay("I cannot unmark anything if you dont give me a number lol.");
-                        continue;
+                        break;
                     }
+
+                    if (unmarkno > taskList.getLength())
+                    {
+                        botSay("There are only " + taskList.getLength() + " tasks, yet you want to unmark task "
+                                + unmarkno + "? Try again bro.");
+                        break;
+                    }
+                    else if (unmarkno < 1)
+                    {
+                        botSay("Your task number to be undone must be positive ahh!!!");
+                        break;
+                    }
+                    Task unmarkedTask = taskList.getTask(unmarkno - 1);
+                    unmarkedTask.setDone(false);
+                    botSay("Ok! I have marked a task as undone:");
+                    botSay(unmarkedTask.toString(), true);
+                    break;
+                case "todo":
+                    if (inputArgs.length < 2){
+                        botSay("You need to tell me what you want to do bro!");
+                        break;
+                    }
+                    Todo newTodo = new Todo(inputArgs[1]);
+                    taskList.addTask(newTodo);
+                    botSay("Alright I added new todo to the list: ");
+                    botSay(newTodo.toString(), true);
+                    botSay("You now have " + taskList.getLength() + " tasks.", true);
+                    break;
+                case "deadline":
+                    if (inputArgs.length < 2){
+                        botSay("Specify what deadline you want me to set!");
+                        break;
+                    }
+
+                    String[] deadlineArgs = inputArgs[1].split(" /by ");
+                    if (deadlineArgs.length < 2){
+                        botSay("Syntax is bad! Describe a deadline, then put \" /by\" followed by the end time.");
+                        break;
+                    }
+
+                    Deadline newDeadline = new Deadline(deadlineArgs[0], deadlineArgs[1]);
+                    taskList.addTask(newDeadline);
+                    botSay("Okay I added new deadline to the list: ");
+                    botSay(newDeadline.toString(), true);
+                    botSay("You now have " + taskList.getLength() + " tasks.", true);
+                    break;
+                case "event":
+                    if (inputArgs.length < 2){
+                        botSay("Specify what kind of event you want me to add!");
+                        break;
+                    }
+
+                    String description;
+                    String[] eventArgs;
+                    Event newEvent;
+
+                    try {
+                        eventArgs = (inputArgs[1].split(" /from "));
+                        description = eventArgs[0];
+                        eventArgs = eventArgs[1].split(" /to ");
+                        newEvent = new Event(description, eventArgs[0], eventArgs[1]);
+                    } catch (Exception e){
+                        botSay("Syntax is bad! Describe an event, put \" /from\" followed by the start time, " +
+                                "then put \" /to\" followed by the end time.");
+                        break;
+                    }
+
+                    taskList.addTask(newEvent);
+                    botSay("Good! Adding new event to the list: ");
+                    botSay(newEvent.toString(), true);
+                    botSay("You now have " + taskList.getLength() + " tasks.", true);
+                    break;
                 default:
-                    Task newTask = new Task(input);
-                    taskList.addTask(newTask);
-                    botSay("Ok! I added a new task:");
-                    System.out.println(PREFIX_INDENT + newTask.toString());
+                    botSay("heh I dun get you bro, type something I understand.");
+                    break;
             }
         }
         botSay("Bye bye! See you later!");
