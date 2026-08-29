f09ab255641343b258cfa305795cb001d0930614
Ray <chan_ray@ymail.com>
2026-08-29 16:09:23 +0800

    Added greeting and exit messages

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Kevie User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Kevie.java
@@ -0,0 +1,21 @@
+public class Kevie {
+    public static void main(String[] args) {
+        String line = "====================================";
+        String banner = """
+                ██╗  ██╗███████╗██╗   ██╗██╗███████╗
+                ██║ ██╔╝██╔════╝██║   ██║██║██╔════╝
+                █████╔╝ █████╗  ██║   ██║██║█████╗
+                ██╔═██╗ ██╔══╝  ╚██╗ ██╔╝██║██╔══╝
+                ██║  ██╗███████╗ ╚████╔╝ ██║███████╗
+                ╚═╝  ╚═╝╚══════╝  ╚═══╝  ╚═╝╚══════╝""";
+        System.out.println(line);
+        System.out.println(banner);
+        System.out.println(line);
+
+        System.out.println("Hey, what's up!");
+        System.out.println("Anything you want to get done today?");
+
+        System.out.println(line);
+        System.out.println("Bye bye! See you later!");
+    }
+}
