186d92a015a6b00615b1f7e2c7841616de73aa2a
Ray <chan_ray@ymail.com>
2026-08-29 17:51:41 +0800

    List item adding and viewing

--- src/main/java/Kevie.java
+++ src/main/java/Kevie.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class Kevie {
 
-    public static final String keviePrefix = "[Kevie]";
-    public static final String userPrefix = "[You]";
+    public static final String KEVIE_PREFIX = "[Kevie]";
+    public static final String USER_PREFIX = "[You]";
+    public static final int LIST_MAX_LEN = 100;
 
     public static void kevieTalk(String msg)
     {
-        System.out.println(keviePrefix + " " + msg);
+        System.out.println(KEVIE_PREFIX + " " + msg);
     }
 
     public static void main(String[] args) {
@@ -25,14 +26,29 @@ public class Kevie {
         kevieTalk("Hey, what's up!");
         kevieTalk("Anything you want to get done today?");
 
+        String[] itemList = new String[LIST_MAX_LEN];
+        int itemListLen = 0;
+
         Scanner scanner = new Scanner(System.in);
         while(true)
         {
-            System.out.print(userPrefix + " ");
+            System.out.print(USER_PREFIX + " ");
             String input = scanner.nextLine();
 
             if (input.equals("bye")) break;
-            kevieTalk(input);
+
+            if (input.equals("list"))
+            {
+                kevieTalk("Here is your list of items: ");
+                for (int i = 0; i < itemListLen; i++) {
+                    System.out.println("  " + (i+1) + ". " + itemList[i]);
+                }
+                continue;
+            }
+
+            itemList[itemListLen] = input;
+            itemListLen++;
+            kevieTalk("Added \"" + input + "\" to your list of items!");
         }
 
         kevieTalk("Bye bye! See you later!");
